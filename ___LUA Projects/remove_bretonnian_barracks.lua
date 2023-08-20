056f0c1384fe50e39b4544c99fabbe9e3cf67b87
LyciaPintella <jessica.ariadne.murphy@gmail.com>
2023-08-20 10:00:33 -0400

    hi

--- ___LUA Projects/remove_bretonnian_barracks.lua
+++ ___LUA Projects/remove_bretonnian_barracks.lua
@@ -1,7 +1,7 @@
 local regions = { "wh3_main_combi_region_couronne", "wh3_main_combi_region_miragliano",
      "wh3_main_combi_region_bordeleaux", "wh3_main_combi_region_lyonesse", "wh3_main_combi_region_copher",
-     "wh3_main_combi_region_zandri" }
-
+     "wh3_main_combi_region_zandri", "wh3_main_combi_region_castle_carcassonne" }
+local function out(t) ModLog("Replace Starting Bretonnian Barracks With Stables: " .. tostring(t) .. ".") end
 local function add_building(slot)
      cm:callback(function()
           cm:region_slot_instantly_upgrade_building(slot, "wh_main_brt_stables_1")
@@ -9,40 +9,58 @@ local function add_building(slot)
 end
 
 local function remove_barracks(region_key)
+     out("Calling function remove_barracks " .. "")
      local region = cm:get_region(region_key)
-     local slot_list = region:slot_list()
-     for i = 0, slot_list:num_items() - 1 do
-          if not slot_list:is_empty() then
-               if slot_list:item_at(i):has_building() then
-                    local building_name = slot_list:item_at(i):building():name()
-                    if string.find(building_name, "wh_main_brt_barracks") then
-                         cm:region_slot_instantly_dismantle_building(slot_list:item_at(i))
+     local slot_list = region:settlement():slot_list()
+     if cm:is_new_game() then
+          for i = 0, slot_list:num_items() - 1 do
+               if not slot_list:is_empty() then
+                    if slot_list:item_at(i):has_building() then
+                         local building_name = slot_list:item_at(i):building():name()
+                         if string.find(building_name, "wh_main_brt_barracks") then
+                              cm:region_slot_instantly_dismantle_building(slot_list:item_at(i))
+                         end
                     end
                end
           end
      end
 end
-remove_barracks("wh3_main_combi_region_desolation_of_drakenmoor")
 
 local function remove_barracks_add_stables(region_key)
+     out("Calling function remove_barracks_add_stables " .. "")
      local region = cm:get_region(region_key)
-     local slot_list = region:slot_list()
-     for i = 0, slot_list:num_items() - 1 do
-          if not slot_list:is_empty() then
-               if slot_list:item_at(i):has_building() then
-                    local building_name = slot_list:item_at(i):building():name()
-                    if string.find(building_name, "wh_main_brt_barracks") then
-                         cm:region_slot_instantly_dismantle_building(slot_list:item_at(i))
-                         add_building(slot_list:item_at(i))
+     local slot_list = region:settlement():slot_list()
+     if cm:is_new_game() then
+          for i = 0, slot_list:num_items() - 1 do
+               if not slot_list:is_empty() then
+                    if slot_list:item_at(i):has_building() then
+                         local building_name = slot_list:item_at(i):building():name()
+                         if string.find(building_name, "wh_main_brt_barracks") then
+                              cm:region_slot_instantly_dismantle_building(slot_list:item_at(i))
+                              add_building(slot_list:item_at(i))
+                         end
                     end
                end
           end
      end
 end
+
 local function loop_through_list()
-     for i = 0, #regions - 1 do
-          local region_key = regions:item_at(i)
+     out("Calling function loop_through_list " .. "")
+     for i, region_key in pairs(regions) do
           remove_barracks_add_stables(region_key)
      end
 end
-loop_through_list()
+
+local function initialize_bretonnian_barracks_replacement()
+     local turn_number = cm:model():turn_number()
+     cm:callback(function()
+          if (turn_number == 1) then
+               loop_through_list()
+               remove_barracks("wh3_main_combi_region_desolation_of_drakenmoor")
+          end
+     end, 5.0)
+end
+cm:add_first_tick_callback_new(function()
+     initialize_bretonnian_barracks_replacement()
+end);
