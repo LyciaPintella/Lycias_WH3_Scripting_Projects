a82997867fa15338b4c3e2814857d470260d3bc1
LyciaPintella <jessica.ariadne.murphy@outlook.com>
2023-11-03 21:59:38 -0400

    Hi

--- ___LUA Projects/!Working On/!!!!scrap.lua
+++ ___LUA Projects/!Working On/!!!!scrap.lua
@@ -10,3 +10,13 @@ core:add_ui_created_callback(
           mixer_add_faction_to_major_faction_list("ovn_vmp_blood_dragons")
      end
 )
+
+
+wh2_dlc15_unit_abilities_mimic_cold_blooded
+wh2_main_lord_abilities_feint_and_reposte
+wh2_dlc15_unit_abilities_mimic_grand_hammer_of_faith
+wh2_dlc15_army_abilities_groms_big_waaagh
+wh_pro04_unit_abilities_loecs_shroud
+wh2_main_faction_abilities_murderous_mastery
+wh2_dlc15_unit_active_mimic_ruinshelter
+wh2_dlc15_army_abilities_waaagh
